41a64d406f8d5c6c00d3dc758e4c63c38d6ff84f
Gina <gina.nikol@gmail.com>
2023-02-12 20:16:53 +0200

    Added OpenAPIDefinition info

--- src/main/java/gina/nikol/demo/ExchangeApplication.java
+++ src/main/java/gina/nikol/demo/ExchangeApplication.java
@@ -3,7 +3,11 @@ package gina.nikol.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Exchange Rates Demo API", description = "A demo API with Spring boot that fetches exchange-rates from the publicly available API https://exchangerate.host.\n"))
 public class ExchangeApplication {
 
 	public static void main(String[] args) {
