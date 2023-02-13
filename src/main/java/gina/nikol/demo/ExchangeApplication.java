19fb7fcf088c91904f33de789a5f312bebb405e8
Gina <gina.nikol@gmail.com>
2023-02-13 11:22:23 +0200

    Corrected gradle dependency for open api documentation

--- src/main/java/gina/nikol/demo/ExchangeApplication.java
+++ src/main/java/gina/nikol/demo/ExchangeApplication.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Exchange Rates Demo API", description = "A demo API with Spring boot that fetches exchange-rates from the publicly available API https://exchangerate.host.\n"))
+@OpenAPIDefinition(info = @Info(title = "Exchange Rates Demo API", description = "A demo API with Spring boot that fetches exchange-rates from the publicly available API, https://exchangerate.host.\n"))
 public class ExchangeApplication {
 
 	public static void main(String[] args) {
